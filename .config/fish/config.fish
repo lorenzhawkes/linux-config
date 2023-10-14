084294ed27d4ae5621609ca2d4fb763c5024b520
lorenzhawkes <lorenzb@gmail.com>
2023-10-14 15:00:15 +0200

    remove old xmonad config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ set -x EDITOR vim
 
 # add local bin
 # add hipcc to the path (for gpu rendering in blender)
-set -x PATH /home/lorenz/.local/bin /opt/rocm-5.4.0/bin $PATH
+set -x PATH /home/lorenz/.local/bin /home/lorenz/dev/apps/zig /opt/rocm-5.4.0/bin $PATH
 
 if test -z (pgrep ssh-agent | string collect)
 	eval (ssh-agent -c)
